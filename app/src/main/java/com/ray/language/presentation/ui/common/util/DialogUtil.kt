e4dfc58ffa131f01a1a0e8d6c0ec4b4f24e1c60e
AjouShama <ajoushama@gmail.com>
2022-04-24 14:06:14 +0900

    [Common] DialogUtil

---
+++ app/src/main/java/com/ray/language/presentation/ui/common/util/DialogUtil.kt
@@ -0,0 +1,46 @@
+package com.ray.language.presentation.ui.common.util
+
+import com.ray.language.presentation.helper.common.modal.alert.AlertDialogFragmentHelper
+import com.ray.language.presentation.ui.common.base.BaseActivity
+import com.ray.language.presentation.ui.common.base.BaseFragment
+import com.ray.language.presentation.ui.common.modal.alert.AlertDialogFragmentContract
+
+fun BaseActivity<*>.showDialog(
+    title: String? = null,
+    message: String? = null,
+    isTwoButton: Boolean = false,
+    cancelMessage: String? = AlertDialogFragmentContract.STRING_CANCEL,
+    confirmMessage: String? = AlertDialogFragmentContract.STRING_CONFIRM,
+    onCancel: (() -> Unit)? = null,
+    onConfirm: (() -> Any)? = null
+) {
+    AlertDialogFragmentHelper.newInstance(
+        title = title,
+        message = message,
+        isTwoButton = isTwoButton,
+        cancelMessage = cancelMessage,
+        confirmMessage = confirmMessage,
+        onCancel = onCancel,
+        onConfirm = onConfirm
+    ).show(supportFragmentManager, tag)
+}
+
+fun BaseFragment<*>.showDialog(
+    title: String? = null,
+    message: String? = null,
+    isTwoButton: Boolean = false,
+    cancelMessage: String? = AlertDialogFragmentContract.STRING_CANCEL,
+    confirmMessage: String? = AlertDialogFragmentContract.STRING_CONFIRM,
+    onCancel: (() -> Unit)? = null,
+    onConfirm: (() -> Any)? = null
+) {
+    AlertDialogFragmentHelper.newInstance(
+        title = title,
+        message = message,
+        isTwoButton = isTwoButton,
+        cancelMessage = cancelMessage,
+        confirmMessage = confirmMessage,
+        onCancel = onCancel,
+        onConfirm = onConfirm
+    ).show(parentFragmentManager, tag)
+}
