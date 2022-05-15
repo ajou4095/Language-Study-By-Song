8a91b1875da857d18213317c7bf34c92a465841e
AjouShama <ajoushama@gmail.com>
2022-05-15 14:37:09 +0900

    [Ui] Util 적용

--- app/src/main/java/com/ray/language/presentation/ui/common/bindingadapter/ImageViewBindingAdapter.kt
+++ app/src/main/java/com/ray/language/presentation/ui/common/bindingadapter/ImageViewBindingAdapter.kt
@@ -3,11 +3,10 @@ package com.ray.language.presentation.ui.common.bindingadapter
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-import com.ray.language.common.util.isNotEmpty
 
 @BindingAdapter("imageUrl")
 fun ImageView.setImageUrl(imageUrl: String?) {
-    if (imageUrl.isNotEmpty) {
+    if (!imageUrl.isNullOrEmpty()) {
         Glide.with(this.context)
             .load(imageUrl)
             .into(this)
